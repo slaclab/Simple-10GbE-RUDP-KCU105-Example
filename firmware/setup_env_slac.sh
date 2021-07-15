605bc6b10c5e81a8d6558896602ea128f68d4493
Larry Ruckman <ruckman@slac.stanford.edu>
2021-07-15 16:38:22 -0700

    adding VCS to setup script

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,2 +1,6 @@
 # Setup the Xilinx software and licensing
 source /afs/slac/g/reseng/xilinx/vivado_2021.1/Vivado/2021.1/settings64.sh
+
+# Setup VCS
+source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/R-2020.12-1/settings.sh
+
