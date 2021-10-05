8329e3e1206ba1f3e06917d8137742460631704f
Larry Ruckman <ruckman@slac.stanford.edu>
2021-10-05 13:17:51 -0700

    updating to rogue v5.10.0

--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -6,5 +6,5 @@ source /afs/slac.stanford.edu/g/reseng/vol26/anaconda/miniconda3/etc/profile.d/c
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v5.8.0
+conda activate rogue_v5.10.0
 #conda activate rogue_pre-release
