464c12b6e5bddf423912816b1320477e7e9c6334
Larry Ruckman <ruckman@slac.stanford.edu>
2022-03-16 17:59:33 -0700

    updating script

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,5 +1,5 @@
 # Setup the Xilinx software and licensing
-source /afs/slac/g/reseng/xilinx/vivado_2021.1/Vivado/2021.1/settings64.sh
+source /afs/slac/g/reseng/xilinx/vivado_2021.2/Vivado/2021.2/settings64.sh
 
 # Setup VCS
 source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/R-2020.12-1/settings.sh
