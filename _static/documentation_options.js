633a15fd3a0bafc0157efc04c476b4c7f426704d
ruck314 <ruck314@users.noreply.github.com>
2024-02-15 22:05:09 +0000

    deploy: 15adf98d246cc6f822b528312458016b9bc43083

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.6.0-7-g8c917d7',
+    VERSION: 'v2.7.0-8-g15adf98',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
