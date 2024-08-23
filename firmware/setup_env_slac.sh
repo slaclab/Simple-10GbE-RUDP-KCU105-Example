3d33656653ab8463dd5c42b2ee782eab05eb9ac2
Larry Ruckman <ruckman@slac.stanford.edu>
2024-08-23 10:12:31 -0700

    updating Vivado to 2024.1

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,5 +1,5 @@
 # Setup the Xilinx software and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/2022.2/Vivado/2022.2/settings64.sh
+source /afs/slac.stanford.edu/g/reseng/xilinx/2024.1/Vivado/2024.1/settings64.sh
 
 # Setup VCS
 source /afs/slac.stanford.edu/g/reseng/synopsys/vcs/T-2022.06-SP2/settings.sh
