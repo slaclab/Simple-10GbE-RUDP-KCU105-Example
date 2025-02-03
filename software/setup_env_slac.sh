933472feb8835c73ecc097a72b61fe0b02668b40
Larry Ruckman <ruckman@slac.stanford.edu>
2025-02-03 13:04:29 -0800

    upgrading to rogue v6.4.4

--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -1,9 +1,9 @@
 ##################################
 # Setup environment
 ##################################
-source /afs/slac.stanford.edu/g/reseng/vol31/anaconda/anaconda3/etc/profile.d/conda.sh
+source /afs/slac.stanford.edu/g/reseng/vol38/anaconda3/etc/profile.d/conda.sh
 
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v6.4.0
+conda activate rogue_v6.4.4
