114da6395310df28688abf82384518d6a366cb3a
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-07 21:34:07 -0800

    updating scripts for SDF

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,5 +1,5 @@
 # Setup the Xilinx software and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/2024.1/Vivado/2024.1/settings64.sh
+source /sdf/group/faders/tools/xilinx/2023.1/Vivado/2023.1/settings64.sh
 
 # Setup VCS
-source /afs/slac.stanford.edu/g/reseng/synopsys/vcs/T-2022.06-SP2/settings.sh
+source /sdf/group/faders/tools/synopsys/vcs/W-2024.09/settings.sh
--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -1,9 +1,9 @@
 ##################################
 # Setup environment
 ##################################
-source /afs/slac.stanford.edu/g/reseng/vol38/anaconda3/etc/profile.d/conda.sh
+source /sdf/group/faders/tools/anaconda3/etc/profile.d/conda.sh
 
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v6.4.4
+conda activate rogue_v6.5.0
