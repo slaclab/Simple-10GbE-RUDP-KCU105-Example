1c7b20080499e947adf6794b7b8468de05b5c1d4
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-26 09:49:06 -0700

    Update to vivado/proc.tcl
    adding ability for custom user proc.tcl to be included with the 'submodule/ruckus/vivado/proc.tcl loading

---
+++ firmware/targets/Simple10GbeRudpKcu105Example/vivado/proc.tcl
@@ -0,0 +1,17 @@
+##############################################################################
+## This file is part of 'Simple-10GbE-RUDP-KCU105-Example'.
+## It is subject to the license terms in the LICENSE.txt file found in the
+## top-level directory of this distribution and at:
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+## No part of 'Simple-10GbE-RUDP-KCU105-Example', including this file,
+## may be copied, modified, propagated, or distributed except according to
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+
+proc my_func1 {{msg ""}} {
+	puts "my_func1: $msg"
+}
+
+proc my_func2 {{msg ""}} {
+	puts "my_func2: $msg"
+}
