bfa21a2bedda1248f20324c05f51517cbc8ce4e9
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-26 09:41:59 -0700

    updating to more portable version of proc.tcl

--- firmware/targets/Simple10GbeRudpKcu105Example/ruckus.tcl
+++ firmware/targets/Simple10GbeRudpKcu105Example/ruckus.tcl
@@ -1,5 +1,5 @@
 # Load RUCKUS environment and library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+source $::env(RUCKUS_PROC_TCL)
 
 # Check for version 2023.1 of Vivado (or later)
 if { [VersionCheck 2023.1] < 0 } {exit -1}
