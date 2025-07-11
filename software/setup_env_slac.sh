8bf1a25fe1e20216d4dfe89ef991345c47fe8668
Larry Ruckman <ruckman@slac.stanford.edu>
2025-07-11 07:35:14 -0700

    updating submodules

--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -6,4 +6,4 @@ source /sdf/group/faders/users/$USER/miniforge3/etc/profile.d/conda.sh
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v6.6.0
+conda activate rogue_v6.6.1
