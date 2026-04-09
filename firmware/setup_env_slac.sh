073b0d33b1e77bbabf51309f620a6efef6d3064e
Larry Ruckman <ruckman@slac.stanford.edu>
2026-04-09 12:58:57 -0700

    updating FW setup

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,5 +1,5 @@
 # Setup the Xilinx software and licensing
-source /sdf/group/faders/tools/xilinx/2025.1/Vivado/2025.1/settings64.sh
+source /sdf/group/faders/tools/xilinx/2025.2/Vivado/2025.2/settings64.sh
 
 # Setup VCS
 source /sdf/group/faders/tools/synopsys/vcs/X-2025.06/settings.sh
