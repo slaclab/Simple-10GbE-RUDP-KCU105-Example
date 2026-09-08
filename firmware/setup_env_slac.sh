c566bed2584bd6f4411e1ef04727c40e9f83d3cc
Larry Ruckman <ruckman@slac.stanford.edu>
2026-09-08 09:45:43 -0700

    updating to latest Vivado/VCS and latest submodules pre-releases

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,5 +1,5 @@
 # Setup the Xilinx software and licensing
-source /sdf/group/faders/tools/xilinx/2025.2/Vivado/2025.2/settings64.sh
+source /sdf/group/faders/tools/xilinx/2026.1/Vivado/2026.1/settings64.sh
 
 # Setup VCS
-source /sdf/group/faders/tools/synopsys/vcs/X-2025.06/settings.sh
+source /sdf/group/faders/tools/synopsys/vcs/Y-2026.03-SP1/settings.sh
